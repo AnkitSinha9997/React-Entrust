a83b9dc92b85ded1e7814afb5b4ebeb2851cea6e
AnkitSinha9997 <sinhaankit234@gmail.com>
2023-07-11 23:45:36 +0530

    prod.js file format

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,13 @@
-const webpack=require("webpack")
-const BundleAnalyzerPlugin=require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const webpack = require('webpack')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
-    plugins:[
-        new webpack.DefinePlugin({
-            "process.env.name":JSON.stringify("Ankit --Production")
-        }),
-        new BundleAnalyzerPlugin()
-    ]
-  }
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Ankit --Production'),
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+}
